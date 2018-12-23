1175773a24de0c501c0cc0fd1ece1101cbb8ce2d
Favour Afolayan <favourafolayan@gmail.com>
2018-12-23 07:18:02 +0100

    included navbar-mobile markup and styles on every page

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,24 @@ let toggle = document.querySelector('#nav-toggle');
 let navbar = document.querySelector('nav#navbar-mobile');
 
 toggle.addEventListener('click', () =>{
-    toggleDisplay();   
+    setTimeout(() => {
+        toggleDisplay();   
+    }, 200);
 });
 
 function toggleDisplay() {
     if (navbar.style.display !== 'block') {
-        return navbar.style.display = 'block';
+        navbar.style.display = 'block';
+        return;
     }
-    return navbar.style.display = 'none';
+    navbar.style.display = 'none';
+    return;
+
+}
+
+function transition() {
+    navbar.style.transitionProperty = 'display';
+    navbar.style.transitionDelay = '1s';
+    navbar.style.transitionDuration = '1s';
 }
 

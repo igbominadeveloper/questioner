36572aed2251fcf5ed32c7b584b5949bd90de2bf
Favour <favourafolayan@gmail.com>
2018-12-26 02:56:01 +0100

    added folder structure to project

---
+++ src/models/Meetup.js
@@ -0,0 +1,33 @@
+let meetups = require('../data/meetups.json');
+
+class Meetups {
+	static all(){
+		return meetups;
+	}
+
+	static find(id){
+		return meetups.find(meetup => meetup.id == id);
+	}
+
+	static create(request){
+		if(request){
+			let meetup = {
+				id : request.id,
+				topic : request.topic,
+				location : request.location,
+				images : request.images ? request.images : [],
+				createdOn : new Date().toLocaleString(),
+				happeningOn : "21-02-2019",
+				tags: request.tags ? request.tags : []
+			};
+			return meetup;
+		}
+		return "No Request was Received";
+	}
+
+	static latest(){
+		return meetups.sort()
+	}
+}
+
+module.exports = Meetups;
---
+++ tests/meetups.spec.js
@@ -0,0 +1,25 @@
+let expect = require('expect');
+let meetup = require('../src/models/Meetup.js');
+
+describe('Meetups', () => {
+	it('returns all created meetups', () => {
+		expect(meetup.all().length > 0).toBe(true);
+	});
+
+	it('can return a specific meetup', () => {
+		expect(meetup.find(1).id).toBe(1);
+	});
+
+	it('can create a new Meetup', () => {
+		let request = {
+			id : request.id,
+			topic : request.topic,
+			location : request.location,
+			images : request.images ? request.images : [],
+			createdOn : new Date().toLocaleString(),
+			happeningOn : "21-02-2019",
+			tags: request.tags ? request.tags : []
+		}
+		expect(meetup.create()).toBeInstanceOf(Object);
+	});
+});

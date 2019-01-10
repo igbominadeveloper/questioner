ae8901b65e983241dc04e1ffa7a93f6477c10d91
Favour <favourafolayan@gmail.com>
2019-01-10 22:21:19 +0100

    [chore #163085717] toggle questions and comments visibility

--- ui/assets/js/app.js
+++ ui/assets/js/app.js
@@ -1,25 +1,57 @@
 const toggle = document.querySelector('#nav-toggle');
-
 const navbar = document.querySelector('nav#navbar-mobile');
+
+const like = document.querySelector('p.like');
+const count = document.querySelector('span.upvote');
+
+
 
 toggle.addEventListener('click', () => {
   setTimeout(() => {
-    toggleDisplay();
+    toggleDisplay(navbar);
   }, 200);
 });
 
-function toggleDisplay() {
-  if (navbar.style.display !== 'block') {
-    navbar.style.display = 'block';
-    navbar.style.animation = 'slide-down .3s ease-in-out';
+function toggleDisplay(selector) {
+  if (selector.style.display !== 'block') {
+    selector.style.display = 'block';
+    selector.style.animation = 'slide-down .3s ease-in-out';
     return;
   }
-  navbar.style.display = 'none';
-  navbar.style.animation = 'slide-down .3s ease-in-out';
+  selector.style.display = 'none';
+  selector.style.animation = 'slide-down .3s ease-in-out';
 }
+
 
 function transition() {
   navbar.style.transitionProperty = 'display';
   navbar.style.transitionDelay = '1s';
   navbar.style.transitionDuration = '1s';
+}
+
+
+function toggleComments(event) {
+  let toggler = event.target;
+  let parent = toggler.parentElement.parentElement.parentElement.parentElement.parentElement;
+  if (parent.children[1].style.display != 'block') {
+    parent.children[1].style.display = 'block';
+    parent.children[2].style.display = 'block';
+    return;
+  }
+  parent.children[1].style.display = 'none';
+  parent.children[2].style.display = 'none';
+}
+
+function increaseCount(event) {
+  let countHolder = event.target.parentNode.lastChild;
+  let count = parseInt(countHolder.innerHTML);
+   count += 1;
+   countHolder.innerHTML = count;
+}
+
+function decreaseCount(event) {
+  let countHolder = event.target.parentNode.lastChild;
+  let count = parseInt(countHolder.innerHTML);
+   count > 0 ? count -= 1 : '';
+   countHolder.innerHTML = count;
 }

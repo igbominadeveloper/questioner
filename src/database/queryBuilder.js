19c13003ce6c9e6fceb08587040316d3b8df8c37
Favour Afolayan <favourafolayan@gmail.com>
2019-01-14 00:57:02 +0100

    Ft connect application to postgresql 163180183 (#51)
    * [chore #163180232] rename data dir to database
    
    * [chore #163180221] create queryBuilder class
    
    * [chore #163180245] install pg package from npm
    
    * [chore

---
+++ src/database/queryBuilder.js
@@ -0,0 +1,22 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL || 'postgres://favour@127.0.0.1:5432/questioner'
+});
+
+class QueryBuilder {
+	run(statement, parameters){
+		return new Promise((resolve, reject) => {
+			pool.query(statement)
+			.then(response => {
+				resolve(response)
+			})
+			.catch(error => {
+				reject(error)
+			})
+		})
+	}
+}
+
+export default QueryBuilder;

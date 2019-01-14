855f7b6abc16b01d3f8e057b62c5337a136e70a7
Favour <favourafolayan@gmail.com>
2019-01-15 00:55:56 +0100

    [chore #163211907] extract tablename to global variable

--- src/models/rsvp.js
+++ src/models/rsvp.js
@@ -1,8 +1,9 @@
 import QueryBuilder from '../database/queryBuilder';
+const table = 'rsvps';
 
 class Rsvp {
   static all(meetupId) {
-    const statement = `SELECT * FROM rsvps WHERE meetupId = $1`;
+    const statement = `SELECT * FROM ${table} WHERE meetupId = $1`;
     return new Promise((resolve, reject) => {
 	    QueryBuilder.run(statement,[meetupId])
 	    .then(response => resolve(response))
@@ -11,9 +12,7 @@ class Rsvp {
   }
 
   static create(rsvp) {
-    rsvps.push(rsvp);
-    helper.writeToFile(filename, rsvps);
-    return rsvps[rsvps.length - 1];
+   
   }
 }
 

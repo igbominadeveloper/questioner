2f613bc6f7d146359c705d1177e1781c3ff9dcd5
Favour <favourafolayan@gmail.com>
2019-01-15 15:42:20 +0100

    [chore #163222231] changed db columns name and fixed ensuing reference error

--- src/database/queryBuilder.js
+++ src/database/queryBuilder.js
@@ -2,24 +2,18 @@ import { Pool } from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
-dotenv.config();
-
 const pool = new Pool({
-	user: process.env.PGUSER,
-	host: process.env.PGHOST,
-	database: process.env.PGDATABASE,
-	password: process.env.PGPASSWORD,
-	port: process.env.PGPORT
+	connectionString: process.env.DATABASE_URL
 });
 
-// pool.on('connect', () => {
-//   console.log('connected to the db');
-// });
+pool.on('connect', () => {
+  console.log('connected to the db');
+});
 
 class QueryBuilder {
 	static run(statement, parameters = ''){
 		return new Promise((resolve, reject) => {
-			pool.query(statement)
+			pool.query(statement,parameters)
 			.then(response => {
 				resolve(response)
 			})

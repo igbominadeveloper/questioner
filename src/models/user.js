77c9843487f6c6fdf26675ac19fb78b4d237768a
Favour <favourafolayan@gmail.com>
2019-01-16 07:53:29 +0100

    [chore ##4200232] added controller method to handle user login

--- src/models/user.js
+++ src/models/user.js
@@ -2,13 +2,14 @@ import QueryBuilder from '../database/queryBuilder';
 import helper from '../helpers/helper';
 
 class User {
-	static login(email, password){
+	static authenticate(email, password){
 		const statement = `SELECT * FROM users where email = $1`;
 		return new Promise((resolve,reject) => {
 			QueryBuilder.run(statement,[email])
 			.then(result => {
-				const token = helper.generateToken(result.rows[0].id);
-				resolve(token);
+				const user = result.rows[0];
+				const token = helper.generateToken(user.id);
+				resolve(user, token);
 			})
 			.catch(error => reject(error))			
 		})

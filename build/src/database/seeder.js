eb3a730add5781f50033122599fef54380d3390c
Favour <favourafolayan@gmail.com>
2019-01-27 20:35:45 +0100

    [chore #163504518] seperated production config from development

--- build/src/database/seeder.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = {
-  users: "\n  INSERT INTO users(firstname,lastname,email,isadmin,password)      \n  VALUES  ('Common','User','user@questioner.com',0,'$2a$10$/RvFN8CxFWnwXwH/CJYZ3.f69ZCWexo1yB9.WTJQyX3pmyhGuoW96'),\n            ('Super','Admin','superadmin@questioner.com',1,'$2a$10$/RvFN8CxFWnwXwH/CJYZ3.f69ZCWexo1yB9.WTJQyX3pmyhGuoW96')\n  "
-};

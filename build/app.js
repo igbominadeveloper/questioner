531b4227871586561b7d8dc441b1a1cf8106a65f
Favour <favourafolayan@gmail.com>
2019-01-31 00:47:15 +0100

    [chore] add cors to app.js

--- build/app.js
+++ build/app.js
@@ -16,11 +16,16 @@ var _routes = require('./src/v1/routes');
 
 var _routes2 = _interopRequireDefault(_routes);
 
+var _cors = require('cors');
+
+var _cors2 = _interopRequireDefault(_cors);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
 
 app.use((0, _morgan2.default)('dev'));
+app.use((0, _cors2.default)());
 app.use(_express2.default.json());
 var port = process.env.PORT || 3000;
 app.use(_routes2.default);

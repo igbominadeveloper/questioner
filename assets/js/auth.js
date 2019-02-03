e7d86072ac1e8fdd19e2ce1a005f5323d809cf2a
Favour <favourafolayan@gmail.com>
2019-02-03 22:48:05 +0100

    [chore] add password confirmation check to form

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -13,7 +13,6 @@ class Authentication{
    * Login a user
    * @param {Object} event 
    * @param {HTMLObjectElement} form
-   * @returns {Object} User 
    */
   static login(event, form){
     event.preventDefault();
@@ -62,6 +61,78 @@ class Authentication{
       .catch(error => console.log(error))
   }
 
+  /**
+   * 
+   * @method register 
+   * @param {event} event 
+   * @param {HTMLObjectElement} form 
+   */
+  static register(event, form){
+    event.preventDefault();
+     let email = form["email"].value;
+     let password = form["password"].value;
+     let password_confirmation = form["password_confirmation"].value;
+     if (password !== password_confirmation) {
+      const errorTemplate = `
+        <div class="alert card">
+          <i class="alert-icon fa fa-warning text-danger p-20"></i>
+          <p class="alert-text text-primary">Passwords don't match</p>
+        </div>
+      `;
+       alertWrapper.classList.remove('hide');
+       alertWrapper.innerHTML = errorTemplate;
+       setTimeout(() => {
+         alertWrapper.classList.add('hide');
+         form["password"].value = '';
+         form["password_confirmation"].value = '';
+       }, 2000);
+       return;
+     }
+     let firstname = form["firstname"].value;
+     let lastname = form["lastname"].value;
+     const data = { email, password, firstname, lastname };
+    fetch(`${apiDomain}/auth/signup`, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+    .then(response => response.json())
+      .then(response => {
+        if(response.error){
+          const errorTemplate = `
+            <div class="alert card">
+              <i class="alert-icon fa fa-warning text-danger p-20"></i>
+              <p class="alert-text text-primary">${ response.error }</p>
+            </div>
+          `;
+          alertWrapper.classList.remove('hide');
+          alertWrapper.innerHTML = errorTemplate;
+          setTimeout(() => {
+            alertWrapper.classList.add('hide');
+            form["password"].value = '';
+          }, 2000);
+        }
+        const user = response.data[0].user;
+        const token = response.data[0].token;
+        const successTemplate = `
+            <div class="alert card">
+              <i class="alert-icon fa fa-check text-primary p-20"></i>
+              <p class="alert-text text-primary">Thanks for signing up ${ user.firstname} ${ user.lastname }</p>
+            </div>
+          `;
+        this.setState(token, user);
+        alertWrapper.classList.remove('hide');
+          alertWrapper.innerHTML = successTemplate;
+          setTimeout(() => {
+            alertWrapper.classList.add('hide');
+          }, 2000);
+      })
+      .catch(error => console.log(error))
+  }
+
   static setState(token, user) {
     if (this.getState() !== '') {
       this.removeState();
@@ -85,7 +156,7 @@ class Authentication{
     if(this.getState() === ''){
       return false;
     }
-    else if (this.getState().length > 0){
+    else if (this.getState()){
       return true;
     }
   }

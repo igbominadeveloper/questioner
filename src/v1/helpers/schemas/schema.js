b8af1babb0d54cb2cc7a8ff9d8e26e5fde6585e7
Favour <favourafolayan@gmail.com>
2019-02-04 19:31:48 +0100

    [chore] add docblocks to Joi validation schema

--- src/v1/helpers/schemas/schema.js
+++ src/v1/helpers/schemas/schema.js
@@ -1,4 +1,9 @@
 import Joi from 'joi';
+/**
+ * set meetup validation schema
+ * 
+ * @constant meetup
+ */
 
 const meetup = Joi.object().keys({
   topic: Joi.string().trim().min(3).required(),
@@ -6,13 +11,31 @@ const meetup = Joi.object().keys({
   date: Joi.date().iso().required(),
 });
 
+/**
+ * set meetupTag validation schema
+ *
+ * @constant meetupTag
+ */
+
 const meetupTag = Joi.object().keys({
   tags: Joi.string().trim().min(2).required(),
 });
 
+/**
+ * set meetupImage validation schema
+ *
+ * @constant meetupImage
+ */
+
 const meetupImage = Joi.object().keys({
   images: Joi.string().trim().min(2).required(),
 });
+
+/**
+ * set question validation schema
+ *
+ * @constant question
+ */
 
 const question = Joi.object().keys({
   user_id: Joi.number().integer().positive().required(),
@@ -21,12 +44,24 @@ const question = Joi.object().keys({
   body: Joi.string().trim().min(3).required(),
 });
 
+/**
+ * set comment validation schema
+ *
+ * @constant comment
+ */
+
 const comment = Joi.object().keys({
   user_id: Joi.number().integer().positive().required(),
   question_id: Joi.number().integer().positive().required(),
   topic: Joi.string().trim().required(),
   comment: Joi.string().trim().required(),
 });
+
+/**
+ * set newUser validation schema
+ *
+ * @constant newUser
+ */
 
 const newUser = Joi.object().keys({
   firstname: Joi.string().trim().min(3).required(),
@@ -35,10 +70,22 @@ const newUser = Joi.object().keys({
   password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
 });
 
+/**
+ * set oldUser validation schema
+ *
+ * @constant oldUser
+ */
+
 const oldUser = Joi.object().keys({
   email: Joi.string().email({ minDomainAtoms: 2 }).required(),
   password: Joi.string().trim().required(),
 });
+
+/**
+ * set userUpdate validation schema
+ *
+ * @constant userUpdate
+ */
 
 const userUpdate = Joi.object().keys({
   firstname: Joi.string().trim().min(3),
@@ -47,10 +94,21 @@ const userUpdate = Joi.object().keys({
   username: Joi.string().trim().min(3),
   phonenumber: Joi.number().integer().positive(),
 });
+/**
+ * set rsvps validation schema
+ *
+ * @constant rsvps
+ */
 
 const rsvps = Joi.object().keys({
   status: Joi.string().trim().valid(['yes', 'no', 'maybe']).required(),
 });
+
+/**
+ * export validation schema object
+ *
+ * @exports {Object} Schema
+ */
 
 export default {
   meetup,

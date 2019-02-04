ac38691ae228d032b68c4145351ce43ae4f6fcc8
Favour <favourafolayan@gmail.com>
2019-02-04 20:58:00 +0100

    [chore] add docblocks to rsvp model

--- src/v1/models/rsvp.js
+++ src/v1/models/rsvp.js
@@ -4,10 +4,33 @@ import queryFactory from '../../database/queryFactory';
 
 import meetup from './Meetup';
 
+/**
+ * set table constant to query except otherwise stated
+ * 
+ * @constant table 
+ */
+
 const table = 'rsvps';
 
 class Rsvp {
-  static all(meetupId) {
+
+/**
+ * ---------------------------------------------------
+ * Rsvp Model
+ * ---------------------------------------------------
+ * 
+ * This Model is responsible for all 
+ * database operations involving 
+ * the rsvps table strictly 
+*/
+
+/**
+ * select all rows for specified id
+ *
+ * @return {Object} Promise
+*/
+
+  static meetupRsvp(meetupId) {
     const statement = `SELECT * FROM ${table} WHERE meetup_id = $1`;
     return new Promise((resolve, reject) => {
       queryFactory.run(statement, [meetupId])
@@ -16,12 +39,31 @@ class Rsvp {
     });
   }
 
+/**
+ * insert new row into table
+ *
+ * @return {Object} Promise
+*/
+
   static async create(rsvp) {
+    /**
+     * use Object destructuring to extract values
+     * 
+     * @constant user_id
+     * @constant meetupId
+     * @constant status
+     * @return {Object} Promise
+     */
+
     const {
       user_id,
       meetup_id,
       status,
     } = rsvp;
+
+    /**
+     * prepare insert statement
+     */
 
     let statement = `INSERT INTO ${table}(user_id,meetup_id,status,topic) VALUES($1, $2, $3, $4) returning *`;
 
@@ -39,5 +81,9 @@ class Rsvp {
     });
   }
 }
+
+/**
+ * export a default object
+ */
 
 export default Rsvp;
